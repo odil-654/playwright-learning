f79ba9d1f1799e9e79a05135be6fce1e60c98f40
odil-654 <odil.isaev@inbox.ru>
2026-06-16 15:27:00 +0500

    Parametrize fixed?

--- test_search_sorting.py
+++ test_search_sorting.py
@@ -14,12 +14,19 @@ class SortFilter(StrEnum):
 
 
 @pytest.mark.parametrize(
-    "name, n, filter_type",
+    "name, n",
     [
-        ("city", 10, SortFilter.LOW_TO_HIGH),
-        ("city", 15, SortFilter.HIGH_TO_LOW),
-        ("habits", 10, SortFilter.LOW_TO_HIGH),
-        ("habits", 15, SortFilter.HIGH_TO_LOW),
+        ("city", 10),
+        ("city", 15),
+        ("habits", 10),
+        ("habits", 15),
+    ],
+)
+@pytest.mark.parametrize(
+    "filter_type",
+    [
+        SortFilter.LOW_TO_HIGH,
+        SortFilter.HIGH_TO_LOW,
     ],
 )
 def test_article_prices_sorting(page, name, n, filter_type):

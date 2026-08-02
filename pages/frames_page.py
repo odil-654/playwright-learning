5a098d96b2524b2d27dcaccd4f16abffd3487938
odil-654 <odil.isaev@inbox.ru>
2026-08-02 16:29:51 +0500

    Task 3 - Final round

--- pages/frames_page.py
+++ pages/frames_page.py
@@ -9,35 +9,35 @@ class FramesPage:
         self.upper_left_text = WebElement(
             page.frame_locator("[name='frame-top']")
             .frame_locator("[name='frame-left']")
-            .locator("*"),
+            .locator("body"),
             "Frames -> Upper left text",
         )
         self.upper_middle_text = WebElement(
             page.frame_locator("[name='frame-top']")
             .frame_locator("[name='frame-middle']")
-            .locator("*"),
+            .locator("body"),
             "Frames -> Upper middle text"
         )
         self.upper_right_text = WebElement(
             page.frame_locator("[name='frame-top']")
             .frame_locator("[name='frame-right']")
-            .locator("*"),
+            .locator("body"),
             "Frames -> Upper right text"
         )
         self.bottom_text = WebElement(
             page.frame_locator("[name='frame-bottom']")
-            .locator("*"),
+            .locator("body"),
             "Frames -> Bottom text"
         )
 
     def get_left_frame_text(self):
-        return self.upper_left_text.get_text_content()
+        return self.upper_left_text.get_inner_text()
 
     def get_right_frame_text(self):
-        return self.upper_right_text.get_text_content()
+        return self.upper_right_text.get_inner_text()
 
     def get_middle_frame_text(self):
-        return self.upper_middle_text.get_text_content()
+        return self.upper_middle_text.get_inner_text()
 
     def get_bottom_text(self):
-        return self.bottom_text.get_text_content()
+        return self.bottom_text.get_inner_text()
